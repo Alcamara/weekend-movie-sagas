1dcb814dc020b14d5beba88244251dd1486cfa48
Al Camara <Alhousseinec@gmail.com>
2022-06-24 22:45:36 -0500

    update movieDetailQuery to include movie ids

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -8,11 +8,16 @@ router.get('/', (req, res) => {
     Get movie title, image, description, and an array of genres
   */
   const movieDetailQuery = `
-    SELECT movies.title, movies.poster, movies.description, array_agg(genres.name) AS genreList FROM movies
+    SELECT movies.id AS movieIds, movies.title, movies.poster, movies.description, array_agg(genres.name) AS genreList FROM movies
     JOIN movies_genres ON movies_genres.movie_id = movies.id
     JOIN genres ON genres.id = movies_genres.genre_id
-    GROUP BY movies.title, movies.poster, movies.description;
+    GROUP BY movieIds, movies.title, movies.poster, movies.description;
   `
+
+  pool.query(movieDetailQuery)
+    .then((results)=>{
+      console.log(results.rows);
+    })
   
 });
 
