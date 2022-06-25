7fcba64e42113a60ef10ce771f6d8c16a448ddb8
Al Camara <Alhousseinec@gmail.com>
2022-06-24 22:36:41 -0500

    create movieDetailQuery variable to get movie title, image, description, and an array of genres

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,16 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  /* 
+    Get movie title, image, description, and an array of genres
+  */
+  const movieDetailQuery = `
+    SELECT movies.title, movies.poster, movies.description, array_agg(genres.name) AS genreList FROM movies
+    JOIN movies_genres ON movies_genres.movie_id = movies.id
+    JOIN genres ON genres.id = movies_genres.genre_id
+    GROUP BY movies.title, movies.poster, movies.description;
+  `
+  
 });
 
 module.exports = router;
