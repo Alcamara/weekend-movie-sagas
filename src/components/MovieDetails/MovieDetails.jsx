93c650a517a609b4c6279ce3cc045897208944b1
Al Camara <Alhousseinec@gmail.com>
2022-06-24 22:05:20 -0500

    added route and MovieDetail to app.js file

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -3,7 +3,13 @@
 export default function MovieDetails(){
     return (
         <div>
-            
+            <h2>Title</h2>
+            <img src="https://dyn1.heritagestatic.com/lf?set=path%5B1%2F6%2F1%2F1%2F1611768%5D&call=url%5Bfile%3Aproduct.chain%5D" height='200' alt="simple pick" />
+            <p>Lorem ipsum dolor sit amet, consectetur
+                 adipisicing elit. Tempora, cum?
+            </p>
+            <h4>Genres</h4>
+            <p>Lorem ipsum dolor sit.</p>
         </div>
     )
 }
