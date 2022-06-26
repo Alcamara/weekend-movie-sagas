c80f97f6379250e17a06ebcc709838415eba2fdb
Al Camara <Alhousseinec@gmail.com>
2022-06-25 22:47:20 -0500

    added card to detail component

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,16 +2,36 @@
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
 export default function Details({movie}){
     return (
         <div>
-            <h2>{movie[0].title}</h2>
-            <img src={movie[0].poster}  alt="simple pick" />
-            <p>{movie[0].description}</p>
-            <h4>Genres</h4>
-            <p>{movie[0].genrelist.map(genre => (
-                genre
-            )).join(', ')}</p>
+            <Card  sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }} >
+                <CardContent>
+                    <Typography sx={{ fontSize: 20 }}>
+                        {movie[0].title}
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                    <img src={movie[0].poster}  alt="simple pick" />
+                </CardContent>
+                <CardContent>
+                    <Typography variant="body2">
+                        {movie[0].description}
+                    </Typography>
+                </CardContent>
+                <CardContent>
+                    <Typography>
+                        Genres
+                    </Typography>
+                    <Typography>
+                        {movie[0].genrelist.map(genre => (
+                        genre
+                    )).join(', ')}
+                    </Typography>
+                </CardContent>
+            </Card>
         </div>
     )
 }
