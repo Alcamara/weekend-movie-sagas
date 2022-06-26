484f4c3b7abd35cf3d87c3f9be978795feab4d4e
Al Camara <Alhousseinec@gmail.com>
2022-06-26 00:18:25 -0500

    added inputs and dropdown for form

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,10 +1,35 @@
 
+//Material UI
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+
+//bootstrap
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 export default function MovieForm(){
     return(
         <div>
             <h3>Movie Form</h3>
-            
+            <form>
+                <div>
+                    <input type="text" placeholder='Movie Title'/>
+                    <input type="text" placeholder='URL Image'/>
+                    <select name="movie-genres" id="genres">
+                        <option value="volvo">Volvo</option>
+                        <option value="saab">Saab</option>
+                        <option value="mercedes">Mercedes</option>
+                        <option value="audi">Audi</option>
+                    </select>
+                </div>
+                
+                
+                
+                
+
+            </form>
         
         </div>
     )
